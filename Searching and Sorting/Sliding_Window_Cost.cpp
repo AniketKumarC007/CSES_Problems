b4252a8a506f91d5d13067adf9ee0e0ba1212195
AniketKumarC007 <fabaniket246@gmail.com>
2024-07-17 23:54:13 +0530

    sliding_window_cost

--- Searching and Sorting/Sliding_Window_Cost.cpp
+++ Searching and Sorting/Sliding_Window_Cost.cpp
@@ -32,16 +32,28 @@ void print(vvi & v)
    return;
 }
 
-void addNum (int a, multiset<int>& left , multiset<int>& right) {
+void print (multiset<int>& mp) {
+    for (auto it : mp) {
+        cout << it  << " ";
+    }
+    cout <<endl ;
+}
+
+void addNum (int a, multiset<int>& left , multiset<int>& right, int &leftsum , int &rightsum) {
 
     left.insert(a) ;
+    leftsum += a ;
     auto it = left.end(); 
     it--; 
+    rightsum += *(left.rbegin())  ;
+    leftsum -= *(left.rbegin()) ;
     right.insert(*(left.rbegin())) ;
     left.erase(it) ;
 
     if (left.size() < right.size()) {
 
+        leftsum += *right.begin() ;
+        rightsum -= *right.begin() ;
         left.insert(*right.begin()) ;
         right.erase(right.begin()) ;
     }
@@ -53,30 +65,54 @@ void solve() {
     vi v (n) ;
     loop ( i ,  0, n)  cin >>v[i] ;
     vi temp ;
+    int leftsum  = 0 , rightsum = 0  ;
     multiset<int> left , right;
     for (int i = 0 ; i <k ; i++) {
-        addNum(v[i] , left , right ) ;
+        addNum(v[i] , left , right , leftsum , rightsum ) ;
     }
     // cout <<"Median rn : " << *left.rbegin() <<endl  ;
     // cout<< *left.rbegin() <<" "  ;
-
-    temp.push_back(*left.rbegin() ) ;
+    // temp.push_back(*left.rbegin() ) ;
+    int median = *left.rbegin()  ;
+    // cout <<"median " <<median <<endl ;
+    cout  << left.size() * median - leftsum  + rightsum - right.size() * median << " " ;
+    
+    // cout << "Left : " <<endl ;
+    // print (left) ;
+    // cout << "Leftsum : " << leftsum <<endl ;
+    // cout << "Right : "<<endl ;
+    // print (right) ;
+    // cout << "Rightsum : " <<rightsum <<endl ;
+    
     int start  = 0 ; 
     for (int i = k; i<n; i++) {
         if (left.find(v[start]) != left.end()) {
             // cout << "Removing from left " <<v[start] << endl ;
+            leftsum -= v[start] ;
             auto it = left.find(v[start]) ;
             left.erase(it) ;
+
         }
         else if (right.find(v[start]) != right.end()) {
             // cout << "Removing from Right " <<v[start] << endl ;
+            rightsum -= v[start] ;
             auto it = right.find(v[start]) ;
             right.erase(it) ;
         }
-        addNum (v[i] , left, right) ;
+        addNum (v[i] , left, right, leftsum, rightsum) ;
         // cout <<"Median rn : " << *left.rbegin() <<endl  ;
         // cout << *left.rbegin() <<" "  ;
-        temp.push_back(*left.rbegin() ) ;
+        // temp.push_back(*left.rbegin() ) ;
+        median = *left.rbegin() ;
+        // cout << "median " <<median <<endl ;
+        cout  << (left.size() * median) - leftsum  + rightsum - (right.size() * median) << " " ;
+        // cout <<endl ;
+        // cout << "Left : " <<endl ;
+        // print (left) ;
+        // cout << "Leftsum : " << leftsum <<endl ;
+        // cout << "Right : "<<endl ;
+        // print (right) ;
+        // cout << "Rightsum : " <<rightsum <<endl ;        
         start++;
     }
     
